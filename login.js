832ae836dbb6c9f39489ef0c829fb4b57fad4722
Elesoo <45455383+Elesoo@users.noreply.github.com>
2019-10-16 18:44:34 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  var menu_noLogin = `<button type="button" class="btn btn-success mr-1" data-toggle="modal" data-target="#modalRegister">Регистрация</button>
+                      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalEnter">Вход</button>`;
+
+  $('.form-inline').append(menu_noLogin);
+});
+
+
+function enter(){
+  var login = $('#inputLogin').val(),
+      pass  = $('#inputPassword').val();
+
+
+  var code = 'Basic ' + btoa(login +':'+ pass);
+
+  $.ajax({
+    url: '/api/login',
+    type: "POST",
+    headers:{
+      'Authorization': code,
+    },
+    success: function () {
+        console.log("Вы вошли");
+    }
+  });
+}
+
+function registration(){
+  var regLogin = $('#regLogin').val(),
+      regName  = $('#regName').val(),
+      regPassword  = $('#regPassword').val();
+
+  $.ajax({
+    url: '/api/registration',
+    type: "POST",
+    data:{
+      'login': regLogin,
+      'name' : regName,
+      'password' : regPassword 
+    },
+    headers: {
+      'Content-Type':'application/json'
+    }
+    success: function () {
+        console.log("Вы зарегистрировались");
+    }
+  });
+}
